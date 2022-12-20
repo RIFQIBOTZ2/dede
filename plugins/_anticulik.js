d2605d78a32123294ed9172f268b04b2f9f9e5a6
RIFQI BOTZ <88196162+RIFQIBOTZ@users.noreply.github.com>
2022-12-20 17:53:54 +0700

    Add files via upload

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -5,7 +5,31 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     // ketika ada yang invite/kirim link grup di chat pribadi
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
-    let teks = `SEWA BOT 1 BULAN 10K
+    let teks = `*BOT ON 24 JAM & ADA FITUR STORE*
+╭──「 *_SEWA BOT_* 」
+│❏  1 MINGGU : IDR 3.000 (3K)
+│❏  1 BULAN : IDR 6.000 (6K)
+│❏  PERMANEN : IDR 10.000 (10K)
+╰────
+╭─ [ KELEBIHAN BOT ]
+│❏  ＯＮ 24 ＪＡＭ
+│❏  *ADA FITUR STORE*
+│❏  FITUR BANYAK
+╰────
+╭─[ KEUNTUNGAN ]
+│❏  ANTI LINK ( AUTO KICK )
+│❏  WELCOME ( OTOMATIS )
+│❏  DAN RATUSAN FITUR LAINYA
+╰────
+╭─[ *MINAT? HUBUNGI NOMOR DI BAWAH* ]
+│❏ wa.me/628560074739?text=sewa+bot
+╰────
+╭─[ *PAYMENT* ]
+│❏  GOPAY
+│❏  DANA
+│❏  QRIS ALL PAYMENT
+╰────
+*_📍 BERGARANSI FULL_*
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -80,8 +80,32 @@ let handler = async (m, { conn }) => {
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-m.reply(`*BOT ON 24 JAM*
-SEWA BOT 1 BULAN 10K`)
+m.reply(`*BOT ON 24 JAM & ADA FITUR STORE*
+╭──「 *_SEWA BOT_* 」
+│❏  1 MINGGU : IDR 3.000 (3K)
+│❏  1 BULAN : IDR 6.000 (6K)
+│❏  PERMANEN : IDR 10.000 (10K)
+╰────
+╭─ [ KELEBIHAN BOT ]
+│❏  ＯＮ 24 ＪＡＭ
+│❏  *ADA FITUR STORE*
+│❏  FITUR BANYAK
+╰────
+╭─[ KEUNTUNGAN ]
+│❏  ANTI LINK ( AUTO KICK )
+│❏  WELCOME ( OTOMATIS )
+│❏  DAN RATUSAN FITUR LAINYA
+╰────
+╭─[ *MINAT? HUBUNGI NOMOR DI BAWAH* ]
+│❏ wa.me/628560074739?text=sewa+bot
+╰────
+╭─[ *PAYMENT* ]
+│❏  GOPAY
+│❏  DANA
+│❏  QRIS ALL PAYMENT
+╰────
+*_📍 BERGARANSI FULL_*
+`)
 }
 
 handler.help = ['sewa']
